cd27c2c786dbf2cd360107cd1f3de66e6514b215
Uday Singh03 <Uday_Singh03@ad.infosys.com>
2016-10-17 23:50:22 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/example/uday_singh03/androidlist/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.uday_singh03.androidlist;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/example/uday_singh03/androidlist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.uday_singh03.androidlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
